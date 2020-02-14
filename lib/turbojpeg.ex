7f8994a1703bf4b94e87ee75660368059f6dc541
Amos L King <amos@binarynoggin.com>
2020-02-13 18:15:17 -0600

    Adds module documentation for Turbojpeg

--- lib/turbojpeg.ex
+++ lib/turbojpeg.ex
@@ -20,6 +20,19 @@ defmodule Turbojpeg do
           height: height()
         }
 
+  @doc """
+  Converts yuv to jpeg images
+
+
+        iex> {:ok, jpeg} = Turbojpeg.yuv_to_jpeg(Shmex.new(frame), 1920, 1080, 90, :I420)
+          {:ok,
+            %Shmex{
+            capacity: 203783,
+            guard: #Reference<0.938325095.2990669827.232440>,
+            name: "/shmex-00000005607042890133#000",
+            size: 203783
+          }}
+  """
   @spec yuv_to_jpeg(Shmex.t(), width, height, quality, format) ::
           {:ok, Shmex.t()} | error()
   def yuv_to_jpeg(yuv, width, height, quality, format) do
@@ -29,6 +42,19 @@ defmodule Turbojpeg do
       {:error, error}
   end
 
+  @doc """
+  Converts jpeg to yuv
+
+
+        iex> {:ok, yuv} = Turbojpeg.jpeg_to_yuv(Shmex.new(jpeg))
+          {:ok,
+            %Shmex{
+            capacity: 203783,
+            guard: #Reference<0.938325095.2990669827.232440>,
+            name: "/shmex-00000005607042890133#000",
+            size: 203783
+          }}
+  """
   @spec jpeg_to_yuv(Shmex.t()) ::
           {:ok, Shmex.t()} | error()
   def jpeg_to_yuv(jpeg) do
@@ -38,6 +64,18 @@ defmodule Turbojpeg do
       {:error, error}
   end
 
+  @doc """
+  Gets the header from a jpegv
+
+       iex> {:ok, header} = Turbojpeg.get_jpeg_header(Shmex.new(jpeg))
+         {:ok,
+           %{
+              format: :I422,
+              width: 192,
+              height: 192
+            }
+         }
+  """
   @spec get_jpeg_header(Shmex.t()) :: {:ok, jpeg_header} | error()
   def get_jpeg_header(jpeg) do
     Native.get_jpeg_header(jpeg)
